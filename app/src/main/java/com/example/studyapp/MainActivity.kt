f34305c9f4a85300e7f504a071c292d3247991d4
ILIIH <jointlord0@gmail.com>
2022-04-03 23:05:00 +0300

    added button and edittext

--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.studyapp
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -17,6 +23,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun clickHandlerFunction(view: View) {
+        val Entered_text = findViewById<EditText>(R.id.editTextTextPersonName)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = Entered_text.text
+        Entered_text.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val doneButton = findViewById<Button>(R.id.done)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+    }
 
 
 }

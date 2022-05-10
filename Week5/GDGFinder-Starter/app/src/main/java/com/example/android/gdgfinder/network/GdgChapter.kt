1037a77412e34831b25cb317002d8bb823fc7964
ILIIH <jointlord0@gmail.com>
2022-05-10 11:36:26 +0300

    init

---
+++ Week5/GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/network/GdgChapter.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.example.android.gdgfinder.network
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class GdgChapter(
+        @Json(name = "chapter_name") val name: String,
+        @Json(name = "cityarea") val city: String,
+        val country: String,
+        val region: String,
+        val website: String,
+        val geo: LatLong
+) : Parcelable
+
+@Parcelize
+data class LatLong(
+        val lat: Double,
+        @Json(name = "lng")
+        val long: Double
+) : Parcelable
+
+@Parcelize
+data class GdgResponse(
+        @Json(name = "filters_") val filters: Filter,
+        @Json(name = "data") val chapters: List<GdgChapter>
+) : Parcelable
+
+@Parcelize
+data class Filter(
+        @Json(name = "region") val regions: List<String>
+) : Parcelable
+

d1e24fd63ea50c74e4ebc15f726327d592823826
ILIIH <jointlord0@gmail.com>
2022-05-11 18:37:39 +0300

    added

--- app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/DevByteApplication.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/DevByteApplication.java
@@ -2,20 +2,18 @@ package com.example.android.devbyteviewer;
 
 import java.lang.System;
 
-/**
- * Override application to setup background work via WorkManager
- */
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0016\u00a8\u0006\u0005"}, d2 = {"Lcom/example/android/devbyteviewer/DevByteApplication;", "Landroid/app/Application;", "()V", "onCreate", "", "app_debug"})
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0006\u001a\u00020\u0007H\u0002J\b\u0010\b\u001a\u00020\u0007H\u0016J\b\u0010\t\u001a\u00020\u0007H\u0002R\u0010\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0004\n\u0002\u0010\u0005\u00a8\u0006\n"}, d2 = {"Lcom/example/android/devbyteviewer/DevByteApplication;", "Landroid/app/Application;", "()V", "applicationScope", "error/NonExistentClass", "Lerror/NonExistentClass;", "delayedInit", "", "onCreate", "setupRecurringWork", "app_debug"})
 public final class DevByteApplication extends android.app.Application {
+    private final error.NonExistentClass applicationScope = null;
     
-    /**
-     * onCreate is called before the first screen is shown to the user.
-     *
-     * Use it to setup any background tasks, running expensive setup operations in a background
-     * thread to avoid delaying app start.
-     */
     @java.lang.Override()
     public void onCreate() {
+    }
+    
+    private final void setupRecurringWork() {
+    }
+    
+    private final void delayedInit() {
     }
     
     public DevByteApplication() {
